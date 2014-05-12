2c546cdd61664f1e888fc402786e9ec42119db00
Daniel Zoller <nosebrain@gmx.net>
2014-05-12 23:55:53 +0200

    added todo

--- src/main/java/de/nosebrain/widget/cafeteria/security/UserdetailsPropertyService.java
+++ src/main/java/de/nosebrain/widget/cafeteria/security/UserdetailsPropertyService.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+@Deprecated // TODO: user inmemory user details manager
 public class UserdetailsPropertyService implements UserDetailsService, InitializingBean {
 
   private static final String ROLE_PREFIX = "ROLE_";
