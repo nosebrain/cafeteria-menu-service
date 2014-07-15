827e6d4606ee4dc950c7ca801e36324780cb3a51
Daniel Zoller <nosebrain@gmx.net>
2014-07-15 23:21:33 +0200

    update to junit 4.11

--- src/test/java/de/nosebrain/widget/cafeteria/parser/KasselParserTest.java
+++ src/test/java/de/nosebrain/widget/cafeteria/parser/KasselParserTest.java
@@ -1,8 +1,8 @@
 package de.nosebrain.widget.cafeteria.parser;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import java.io.InputStream;
 import java.util.List;
