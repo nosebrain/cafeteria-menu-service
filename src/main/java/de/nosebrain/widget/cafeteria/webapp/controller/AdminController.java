c32e4aefa87ccf4fa958caf5c12a00548eae898b
Daniel Zoller <5073757+nosebrain@users.noreply.github.com>
2016-01-21 23:37:17 +0100

    added admin index page (lists all configured cafeterias)

--- src/main/java/de/nosebrain/widget/cafeteria/webapp/controller/AdminController.java
+++ src/main/java/de/nosebrain/widget/cafeteria/webapp/controller/AdminController.java
@@ -1,11 +1,14 @@
 package de.nosebrain.widget.cafeteria.webapp.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import de.nosebrain.widget.cafeteria.model.config.UniversityInfo;
 import de.nosebrain.widget.cafeteria.service.CafeteriaStore;
 
 @Controller
@@ -13,6 +16,15 @@ public class AdminController {
   
   @Autowired
   private CafeteriaStore cafeteriaStore;
+  
+  @Autowired
+  private Map<String, UniversityInfo> universityInfo;
+  
+  @RequestMapping("/admin")
+  public String getAdminOverview(final Model model) {
+    model.addAttribute("infos", this.universityInfo);
+    return "admin/index";
+  }
   
   @RequestMapping("/admin/cache")
   public String getAdminCacheOverview(final Model model, @RequestParam(defaultValue = "10") final int limit) {
